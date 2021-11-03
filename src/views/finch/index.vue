6279145e12c6e38b5fb5677dcfc23c40f5d7762d
jfx1973 <1593460576@qq.com>
2021-11-03 22:13:44 +0800

    洗篮子菜单

--- src/views/finch/index.vue
+++ src/views/finch/index.vue
@@ -1,57 +1,37 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
-      </el-form-item>
-    </el-form>
+    <el-button type="text" @click="dialogFormVisible = true">新增玩家</el-button>
+    <el-dialog title="玩家信息" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="玩家描述" :label-width="formLabelWidth">
+          <el-input v-model="form.desc" autocomplete="off"></el-input>
+        </el-form-item>
+        <!--<el-form-item label="" :label-width="formLabelWidth">-->
+          <!--<el-select v-model="form.region" placeholder="请选择活动区域">-->
+            <!--<el-option label="区域一" value="shanghai"></el-option>-->
+            <!--<el-option label="区域二" value="beijing"></el-option>-->
+          <!--</el-select>-->
+        <!--</el-form-item>-->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="onCancel = false">取 消</el-button>
+        <el-button type="primary" @click="onSubmit()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
   export default {
     data() {
       return {
+        dialogTableVisible: false,
+        dialogFormVisible: false,
         form: {
           name: '',
+          desc: '',
           region: '',
           date1: '',
           date2: '',
@@ -59,13 +39,21 @@
           type: [],
           resource: '',
           desc: ''
-        }
-      }
+        },
+        formLabelWidth: '120px'
+      };
     },
     methods: {
       onSubmit() {
-        console.log('submit!');
+        this.$message('submit!')
+
+      },
+      onCancel() {
+        this.$message({
+          message: 'cancel!',
+          type: 'warning'
+        })
       }
     }
-  }
+  };
 </script>
