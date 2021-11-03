06210e09682186ed85b05b3b4f6542a87cf6c749
JerAxxxxxxx <q14341319@gmail.com>
2021-11-03 22:18:36 +0800

    Update index.vue

--- src/views/finch/index.vue
+++ src/views/finch/index.vue
@@ -10,50 +10,52 @@
           <el-input v-model="form.desc" autocomplete="off"></el-input>
         </el-form-item>
         <!--<el-form-item label="" :label-width="formLabelWidth">-->
-          <!--<el-select v-model="form.region" placeholder="请选择活动区域">-->
-            <!--<el-option label="区域一" value="shanghai"></el-option>-->
-            <!--<el-option label="区域二" value="beijing"></el-option>-->
-          <!--</el-select>-->
+        <!--<el-select v-model="form.region" placeholder="请选择活动区域">-->
+        <!--<el-option label="区域一" value="shanghai"></el-option>-->
+        <!--<el-option label="区域二" value="beijing"></el-option>-->
+        <!--</el-select>-->
         <!--</el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="onCancel = false">取 消</el-button>
+        <el-button @click="onCancel()">取 消</el-button>
         <el-button type="primary" @click="onSubmit()">确 定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        dialogTableVisible: false,
-        dialogFormVisible: false,
-        form: {
-          name: '',
-          desc: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        formLabelWidth: '120px'
-      };
-    },
-    methods: {
-      onSubmit() {
-        this.$message('submit!')
-
+export default {
+  data() {
+    return {
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        desc: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: ''
       },
-      onCancel() {
-        this.$message({
-          message: 'cancel!',
-          type: 'warning'
-        })
-      }
+      formLabelWidth: '120px'
     }
-  };
+  },
+  methods: {
+    onSubmit() {
+      this.$message('submit!')
+      this.dialogFormVisible = false
+      this.dialogTableVisible = false
+    },
+    onCancel() {
+      this.dialogFormVisible = false
+      this.dialogTableVisible = false
+      this.$message({
+        message: 'cancel!',
+        type: 'warning'
+      })
+    }
+  }
+}
 </script>
