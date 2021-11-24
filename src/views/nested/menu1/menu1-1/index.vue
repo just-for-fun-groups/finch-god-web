c0a265fdf8c247a2a8e809bf26ca2ffb95dd53b0
jiangfengxiao <1593460576@qq.com>
2021-11-24 18:30:31 +0800

    轮播大小自适应 设置失败 谁研究一下

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -1,7 +1,79 @@
 <template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 1-1" type="success">
-      <router-view />
-    </el-alert>
-  </div>
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+    v-for="{{item}}">
+    <el-table-column
+      prop="id"
+      label="序号"
+      width="100%">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="100%">
+    </el-table-column>
+    <el-table-column
+      prop="signature"
+      label="标志性B话"
+      width="100%">
+    </el-table-column>
+    <el-table-column
+      prop="createTime"
+      label="创建时间"
+      width="100%">
+    </el-table-column>
+    <el-table-column
+      prop="updateTime"
+      label="最近更改时间"
+      width="100%">
+    </el-table-column>
+  </el-table>
 </template>
+
+<style>
+  .el-table .warning-row {
+    background: oldlace;
+  }
+
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
+</style>
+
+<script>
+  export default {
+    methods: {
+      tableRowClassName({row, rowIndex}) {
+        if (rowIndex === 1) {
+          return 'warning-row';
+        } else if (rowIndex === 3) {
+          return 'success-row';
+        }
+        return '';
+      }
+    },
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }]
+      }
+    }
+  }
+</script>
